e58357c32d4f639877c57ec5f473894b7911f86c
Guilherme Melnik Trombetta <melnik.trombetta@gmail.com>
2025-10-27 21:56:31 -0300

    Versão R1

--- src/vendas/Venda.java
+++ src/vendas/Venda.java
@@ -1,9 +1,7 @@
 package vendas;
 
 import clientes.Cliente;
-
 import java.util.ArrayList;
-
 import java.util.List;
 
 public class Venda {
@@ -28,7 +26,11 @@ public class Venda {
         return cliente;
     }
 
-    public List<ItemVenda> getItens() {
+    public java.util.List<ItemVenda> getItens() {
         return itens;
+    }
+
+    public void adicionarItem(ItemVenda item) {
+        itens.add(item);
     }
 }
